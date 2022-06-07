199a64fe37f3db5242f0e05ab85c397f10f3b527
OlleLeonard <olleleonard@gmail.com>
2022-06-07 16:05:34 +0200

    pokemonster

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import UserData from "./Data/User";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './index.css';
+import Account from './Account';
+import Start from './Start';
+import Home from "./Home";
+import Choose from './Choose';
+import Rival from './Rival';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  const [feedback, setFeedback] = useState(UserData);
+
+console.log(feedback);
+
+  return (
+
+    <BrowserRouter>
+    <div className="container">
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/Account" element={<Account />} />
+    <Route path="/Start" element={<Start />} />
+    </Routes>
+    </div>
+    <div>
+    <Routes>
+    <Route path="/Choose" element={<Choose />} />
+    <Route path="/Rival" element={<Rival />} />
+    </Routes>
+    </div>
+    </BrowserRouter>
+  );
+  }
+
+
+
+
+  export default App;
---
+++ src/Data/User.js
@@ -0,0 +1,20 @@
+
+const UserData = [
+    {
+        id: 1,
+        rating: 10,
+        text: "hej",
+    },
+    {
+        id: 2,
+        rating: 5,
+        text: "på",
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: "dig",
+    },
+];
+
+export default UserData;
